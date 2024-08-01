c5539583163ce093873ee48f1e12a3a9ad4f4a07
laisfrr <laisf.oliv@gmail.com>
2024-08-01 16:31:58 -0300

    feat: implementacao da rota usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -30,3 +30,71 @@ usuariosRouter.get('/usuarios/:idUsuario', async (req, res) => {
     return res.status(500).json('um erro aconteceu', error)
   }
 })
+
+// cadastrar usuario
+
+usuariosRouter.post('/usuarios', async (req, res) => {
+  const { nome, cpf, email, telefone, dataNasc, endereco } = req.body
+
+  try {
+    const buscarEmail = await Usuario.findOne({ where: { email: email } })
+    const buscarCpf = await Usuario.findOne({ where: { cpf: cpf } })
+
+    if (buscarEmail || buscarCpf) {
+      return res.status(404).json({ message: 'Email e/ou Cpf já cadastrado!' })
+    }
+
+    const novoUsuario = {
+      nome,
+      cpf,
+      email,
+      telefone,
+      dataNasc,
+      endereco
+    }
+
+    await Usuario.create(novoUsuario, { include: [Endereco] })
+    return res.status(201).json(novoUsuario)
+  } catch (error) {
+    return res.status(500).json('um erro aconteceu', error)
+  }
+})
+
+//atualizar usuario
+
+usuariosRouter.put('/usuarios/:idUsuario', async (req, res) => {
+  const { nome, cpf, email, telefone, dataNasc, endereco } = req.body
+  const { idUsuario } = req.params
+
+  try {
+    const usuario = await Usuario.findByPk(idUsuario)
+
+    if (!usuario) {
+      return res.status(404).json({ message: 'Usuário não encontrado!' })
+    }
+
+    await Endereco.update(endereco, { where: { usuarioId: idUsuario } })
+    await Usuario.update({ nome, cpf, email, telefone, dataNasc })
+    return res.status(204).json()
+  } catch (error) {
+    return res.status(500).json('um erro aconteceu', error)
+  }
+})
+
+//deletar Usuario
+
+usuariosRouter.delete('/usuarios/:idUsuario', async (req, res) => {
+  const { idUsuario } = req.params
+
+  try {
+    const usuario = await Usuario.findByPk(idUsuario)
+    if (!usuario) {
+      return res.status(404).json({ message: 'Usuário não encontrado!' })
+    }
+
+    await usuario.destroy()
+    return res.status(200).json({ message: 'Usuário removido com sucesso!' })
+  } catch (error) {
+    return res.status(500).json('um erro aconteceu', error)
+  }
+})
